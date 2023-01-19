f18b634da9db5417b402c02378f3b845641d1c7b
angelo <57320780+angelogelo@users.noreply.github.com>
2023-01-19 15:48:43 +0800

    Update index.php

--- index.php
+++ index.php
@@ -148,6 +148,32 @@
         </div>
     </section>
 
+    <!-- Announcement -->
+    <section class="class section" id="class">
+        <div class="container">
+            <div class="row">
+
+                    <div class="col-lg-12 col-12 text-center mb-5">
+                        <h2 data-aos="fade-up" data-aos-delay="200">Announcements</h2>
+                        </div>
+                    <?php
+                        $announcement = $connection->query("SELECT * FROM announcement");
+                        while($announcementRow = $announcement->fetch_array()){
+                    ?>
+                    <div class="col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="400">
+                        <div class="class-thumb">
+                            <div class="class-info">
+                                <h3 class="mb-1"><?= $announcementRow['title'] ?></h3>
+
+                                <p class="mt-3"><?= $announcementRow['description'] ?></p>
+                            </div>
+                        </div>
+                    </div>
+                    <?php } ?>
+            </div>
+        </div>
+     </section>
+
 
      <!-- ABOUT -->
      <section class="about section" id="about">
@@ -171,38 +197,6 @@
                     </div>
                </div>
      </section>
-
-
-     <!-- CLASS -->
-     <!-- <section class="class section" id="class">
-               <div class="container">
-                    <div class="row">
-
-                            <div class="col-lg-12 col-12 text-center mb-5">
-                                <h6 data-aos="fade-up">Mission</h6>
-
-                                <h2 data-aos="fade-up" data-aos-delay="200">Our Training Classes</h2>
-                             </div>
-
-                            <div class="col-lg-4 col-md-6 col-12" data-aos="fade-up" data-aos-delay="400">
-                                <div class="class-thumb">
-                                    <img src="website-assets/images/class/yoga-class.jpg" class="img-fluid" alt="Class">
-
-                                    <div class="class-info">
-                                        <h3 class="mb-1">Yoga</h3>
-
-                                        <span><strong>Trained by</strong> - Bella</span>
-
-                                        <span class="class-price">$50</span>
-
-                                        <p class="mt-3">Lorem ipsum dolor sit amet, consectetur adipiscing</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                    </div>
-               </div>
-     </section> -->
 
 
      <!-- SCHEDULE -->
